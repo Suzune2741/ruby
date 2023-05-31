4b7a8ea9ce14cd10fbe311f2dc15a2df927b40fb
Suzune2741 <kannra1174@gmail.com>
2023-05-31 16:40:08 +0900

    2分探索木なしで表示まで

--- kadai-2.rb
+++ kadai-2.rb
@@ -1,16 +1,38 @@
-def readfile(file)
-    return file.read
+
+def resdata(data)
+    pat=/(\d{4})(\d{2})(\d{2})/
+    res=data[2].match(pat)
+    puts "マッチしたデータはこちらです。"
+    puts "名前:#{data[0]}"
+    puts "E-mail:#{data[3]}"
+    puts "誕生日:#{res[1]}年#{res[2]}月#{res[3]}日"
+    puts "年齢:#{data[1]}"
+    puts "以上です。"
+end
+i=0
+data=[]
+hash=[]
+#ファイルから読み込み
+file=File.open("meibo_10.csv","r")
+while data[i]=file.gets
+    val=data[i].split(',')
+    hash[i]=val[0]
+    i+=1;
 end
 
-def resdata(name,email,birth,age)
-    puts "name:#{name}"
-    puts "E-mail:#{email}"
-    puts "birthday:#{birth}"
-    puts "age:#{age}"
+
+
+##検索
+print "name(ローマ字):"
+names=gets.chop
+hlen=hash.length
+hlen.times do |i|
+    if(names==hash[i])
+        resdata(val)
+    end
 end
-
-file=File.open("meibo.csv","r")
-
-name=readfile(file)
-puts name
-
+hlen.times do |i|
+    hash[hlen/2]
+end
+#ファイルを閉じる
+file.close
